f37f2459c1e379069c184f0cbf03036ed46338dc
Dan Snel <deltasoft7@gmail.com>
2026-09-19 08:17:55 +0300

    first

---
+++ main.js
@@ -0,0 +1,40 @@
+const { app, BrowserWindow } = require('electron')
+
+function createWindow() {
+    const win = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            nodeIntegration: true
+        },
+        resizable: true,
+        minWidth: 400,    // Minimum width allowed
+        minHeight: 300,   // Minimum height allowed
+        maxWidth: 1200,   // Maximum width allowed
+        maxHeight: 900,   // Maximum height allowed
+    })
+
+    win.loadFile('index.html')
+}
+
+app.whenReady().then(() => {
+    createWindow()
+
+    app.on('activate', () => {
+        if (BrowserWindow.getAllWindows().length === 0) {
+            createWindow()
+        }
+    })
+})
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit()
+    }
+})
+
+app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+        createWindow()
+    }
+})
